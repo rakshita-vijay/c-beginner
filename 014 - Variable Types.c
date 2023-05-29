fa8d9080b550b48ec9a8f484a557929577fb829c
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2023-05-29 09:17:14 +0530

    Create 014 - Variable Types.c

---
+++ 014 - Variable Types.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    float b;
+    short c;
+    long d;
+    double e;
+    long long f;
+    long double g;
+    char h;
+
+    printf("a is of size: %i \n", sizeof(a));
+    printf("b is of size: %i \n", sizeof(b));
+    printf("c is of size: %i \n", sizeof(c));
+    printf("d is of size: %i \n", sizeof(d));
+    printf("e is of size: %i \n", sizeof(e));
+    printf("f is of size: %i \n", sizeof(f));
+    printf("g is of size: %i \n", sizeof(g));
+    printf("h is of size: %i \n", sizeof(h));
+}
