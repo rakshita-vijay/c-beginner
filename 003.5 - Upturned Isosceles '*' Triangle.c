8df9147f533a0e073376a9d81da25646ff4eb466
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2023-05-29 09:07:36 +0530

    Create 003.5 - Upturned Isosceles '*' Triangle.c

---
+++ 003.5 - Upturned Isosceles '*' Triangle.c
@@ -0,0 +1,42 @@
+/*
+
+O/P:
+*************
+ ***********
+  *********
+   *******
+    *****
+     ***
+      *
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int numberOfLines = 0;
+
+    printf("Enter the number of lines to print: ");
+    scanf("%i", &numberOfLines);
+
+    for(int lineNumber = numberOfLines ; lineNumber > 0 ; lineNumber--)
+    {
+        for(int numberOfSpaces = 0 ; numberOfSpaces < (numberOfLines-lineNumber) ; numberOfSpaces++)
+        {
+            printf(" ");
+        }
+
+        for(int numberOfAsterisks = numberOfLines ; numberOfAsterisks > (numberOfLines-lineNumber) ; numberOfAsterisks--)
+        {
+            printf("*");
+        }
+
+        for(int numberOfAsterisks = numberOfLines-1 ; numberOfAsterisks > (numberOfLines-lineNumber) ; numberOfAsterisks--)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
