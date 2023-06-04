9f5cf2fbf4be308eb002a7580e7d469db3229785
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2023-06-04 09:59:27 +0530

    Add clang formatter config file

--- 002_perimeter_and_area_of_rectangle.c
+++ 002_perimeter_and_area_of_rectangle.c
@@ -2,32 +2,29 @@
 
 #include <stdio.h>
 
-float PerimeterOfRectangle(float LengthOfSide, float WidthOfSide)
-{
-    return 2.0*(LengthOfSide+WidthOfSide);
+float PerimeterOfRectangle(float LengthOfSide, float WidthOfSide) {
+  return 2.0 * (LengthOfSide + WidthOfSide);
 }
 
-float AreaOfRectangle(float LengthOfSide, float WidthOfSide)
-{
-    return LengthOfSide*WidthOfSide;
+float AreaOfRectangle(float LengthOfSide, float WidthOfSide) {
+  return LengthOfSide * WidthOfSide;
 }
 
-int main()
-{
-    float LengthOfSide, WidthOfSide;
-    float perimeter, area;
+int main() {
+  float LengthOfSide, WidthOfSide;
+  float perimeter, area;
 
-    printf("Enter LengthOfSide: ");
-    scanf("%f", &LengthOfSide);
-    printf("\n");
+  printf("Enter LengthOfSide: ");
+  scanf("%f", &LengthOfSide);
+  printf("\n");
 
-    printf("Enter WidthOfSide: ");
-    scanf("%f", &WidthOfSide);
-    printf("\n");
+  printf("Enter WidthOfSide: ");
+  scanf("%f", &WidthOfSide);
+  printf("\n");
 
-    perimeter = PerimeterOfRectangle(LengthOfSide, WidthOfSide);
-    area = AreaOfRectangle(LengthOfSide, WidthOfSide);
+  perimeter = PerimeterOfRectangle(LengthOfSide, WidthOfSide);
+  area = AreaOfRectangle(LengthOfSide, WidthOfSide);
 
-    printf("Perimeter Of Rectangle is: %f \n", perimeter);
-    printf("Area Of Rectangle is: %f \n", area);
+  printf("Perimeter Of Rectangle is: %f \n", perimeter);
+  printf("Area Of Rectangle is: %f \n", area);
 }
