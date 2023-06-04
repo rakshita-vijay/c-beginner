e0748184f395a0fba2614315fbe8728b79859762
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2023-06-04 08:53:47 +0530

    multiple to single 015

--- 015_gross_pay_taxes_net_pay.c
+++ 015_gross_pay_taxes_net_pay.c
@@ -1,18 +1,55 @@
-// without functions
+// with functions
 
 #include <stdio.h>
+
+float calcGrossPay(float numberOfHoursWorked)
+{
+    float hourlyPayUnderFortyHours = 12.0;
+    float hourlyPayAboveFortyHours = 18.0;
+
+    if (numberOfHoursWorked <= 40.0)
+    {
+        return (numberOfHoursWorked*hourlyPayUnderFortyHours);
+    }
+
+    else if (numberOfHoursWorked > 40.0)
+    {
+        return (40.0*hourlyPayUnderFortyHours)+((numberOfHoursWorked-40.0)*hourlyPayAboveFortyHours);
+    }
+}
+
+float calcTaxes(float numberOfHoursWorked)
+{
+    float grossPay = calcGrossPay(numberOfHoursWorked);
+    float taxFirstThreeHun = 0.15;
+    float taxNextOneFifty = 0.2;
+    float taxRest = 0.25;
+
+    if (grossPay <= 300)
+    {
+        return grossPay*taxFirstThreeHun;
+    }
+
+    else if (grossPay>300 && grossPay<=450)
+    {
+        return (300*taxFirstThreeHun)+((grossPay-300)*taxNextOneFifty);
+    }
+
+    else if (grossPay > 450)
+    {
+        // taxes = ((300*taxFirstThreeHun) + (150*taxNextOneFifty) + ((grossPay-450)*taxRest));
+        return 75+((grossPay-450)*taxRest);
+    }
+}
+
+float calcNetPay(float numberOfHoursWorked)
+{
+    return calcGrossPay(numberOfHoursWorked)-calcTaxes(numberOfHoursWorked);
+}
 
 int main()
 {
     float numberOfHoursWorked;
-    int hourlyPayUnderFortyHours = 12;
-    int hourlyPayAboveFortyHours = 18;
-    float taxFirstThreeHun = 0.15;
-    float taxNextOneFifty = 0.2;
-    float taxRest = 0.25;
-    float grossPay;
-    float taxes;
-    float netPay;
 
     printf("Enter the number of hours a person has worked: ");
     scanf("%f", &numberOfHoursWorked);
@@ -20,39 +57,11 @@ int main()
 
     // numberOfHoursWorked = 30.0; // 40*12 + 5*18 = 570
 
+    printf("\n~*~*~*~*~*~*~*~*~*~*~*~*~ \n \n");
+
     printf("Number of Hours Worked: %.2f \n", numberOfHoursWorked);
 
-    if (numberOfHoursWorked <= 40.0)
-    {
-        grossPay = (numberOfHoursWorked*hourlyPayUnderFortyHours);
-        printf("Gross Pay: %.2f \n", grossPay);
-    }
-
-    else if (numberOfHoursWorked > 40.0)
-    {
-        grossPay = ((40.0*hourlyPayUnderFortyHours) + ((numberOfHoursWorked-40.0)*hourlyPayAboveFortyHours));
-        printf("Gross Pay: %.2f \n", grossPay);
-    }
-
-    if (grossPay <= 300)
-    {
-        taxes = (grossPay * taxFirstThreeHun);
-        printf("Taxes: %.2f \n", taxes);
-    }
-
-    else if (grossPay>300 && grossPay<=450)
-    {
-        taxes = ((300*taxFirstThreeHun) + ((grossPay-300)*taxNextOneFifty));
-        printf("Taxes: %.2f \n", taxes);
-    }
-
-    else if (grossPay > 450)
-    {
-        // taxes = ((300*taxFirstThreeHun) + (150*taxNextOneFifty) + ((grossPay-450)*taxRest));
-        taxes = 75 + ((grossPay-450)*taxRest);
-        printf("Taxes: %.2f \n", taxes);
-    }
-
-    netPay = (grossPay - taxes);
-    printf("NetPay: %.2f \n", netPay);
+    printf("Gross Pay: %.2f \n", calcGrossPay(numberOfHoursWorked));
+    printf("Taxes: %.2f \n", calcTaxes(numberOfHoursWorked));
+    printf("NetPay: %.2f \n", calcNetPay(numberOfHoursWorked));
 }
