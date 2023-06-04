8339ddb415463a0b38133994ca6ee4e62e9bd007
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2023-06-04 08:50:14 +0530

    multiple to single 013

--- 013_converting_minutes_to_days_and_years.c
+++ 013_converting_minutes_to_days_and_years.c
@@ -1,31 +1,48 @@
-// without functions
+// with functions
 
 #include <stdio.h>
+
+float Days(float mins)
+{
+    return (mins/(60.0*24.0));
+}
+
+float Years(float mins)
+{
+    return (Days(mins)/365.0);
+}
+
+float RestOfMins(float mins)
+{
+    return (Days(mins) - ((int) Days(mins)))*1440;
+}
+
+float RestOfDays(float mins)
+{
+    return (Years(mins) - ((int) Years(mins)))*365;
+}
 
 int main ()
 {
     float mins;
     float days;
-    float lowestMultOf1440;
     float restOfMins;
     float years;
     float restOfDays;
 
     printf("Enter the number of minutes: ");
-    scanf("%f", &mins);
+    scanf("%f", &mins);                                                      // mins = 525600.0 + 1440.0;
     printf("\n");
-
-    // mins = 525600.0 + 1440.0;
 
     printf("Number of minutes entered is: %f \n", mins);
 
-    days = (mins/(60.0*24.0)); //1440
-    restOfMins = ceil((days - ((int) days))*1440);
+    // days = Days(mins); //1440
+    // restOfMins = RestOfMins(mins);
 
-    printf ("Number of minutes entered is: %i days and %f minutes \n", ((int) days), restOfMins);
+    printf ("Number of minutes entered is: %i days and %.2f minutes \n", ((int) Days(mins)), RestOfMins(mins));
 
-    years =  (mins/(60.0*24.0*365.0)); //525600
-    restOfDays = ceil((years - ((int) years))*365);
+    // years =  Years(mins); //525600
+    // restOfDays = RestOfDays(mins);
 
-    printf ("Number of minutes entered is: %i years and %f days \n", ((int) years), restOfDays);
+    printf ("Number of minutes entered is: %i years and %.5f days \n", ((int) Years(mins)), RestOfDays(mins));
 }
