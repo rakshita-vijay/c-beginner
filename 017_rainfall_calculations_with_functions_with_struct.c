19cd08364f861c3c82123df2817d635801795b5d
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2023-06-04 08:58:02 +0530

    double to single 017

--- 017_rainfall_calculations_with_functions_with_struct.c
+++ 017_rainfall_calculations_with_functions_with_struct.c
@@ -1,74 +1,232 @@
-// with functions, without struct
+// with functions, with struct
 
 #include <stdio.h>
-#include <string.h>
 
-float totalRainfallPerYearAndAverageRainfallPerMonthIn1Year(int years[5], char months[], float arr[5][12]);
-float averageRainfallPerMonthIn1Year(float totalPerYear);
-float printTRPYAARPMI1Y(int years, float totalPerYear, float averagePerMonth);
-float printAverageRainfallPerYear(int years[5], float sumForAveragePerMonth);
-float averageRainfallPerMonthAcross5Years(char months[], float averagePerMonthAcross5YearsArr[], float arr[5][12]);
-
-float totalRainfallPerYearAndAverageRainfallPerMonthIn1Year(int years[5], char months[], float arr[5][12])
+struct rainfall12M
 {
-    int outerCount;
-    int innerCount;
-    float sumForTotalPerYear = 0.0;
+    float inch1;
+    float inch2;
+    float inch3;
+    float inch4;
+    float inch5;
+    float inch6;
+    float inch7;
+    float inch8;
+    float inch9;
+    float inch10;
+    float inch11;
+    float inch12;
+};
+
+float calcTotalRainfallPerYear(struct rainfall12M year2k);
+void printTotalRainfallPerYear(int years[5], struct rainfall12M arrayRainfall[]);
+
+float calcAverageRainfallPerMonthIn1Year(struct rainfall12M year2k);
+void printAverageRainfallPerMonthIn1Year(int years[5], struct rainfall12M arrayRainfall[]);
+
+float calcAverageRainfallPerYear(struct rainfall12M arrayRainfall[]);
+void printAverageRainfallPerYear(int years[5], struct rainfall12M arrayRainfall[]);
+
+float calcAverageRainfallPerMonthAcross5Years(int monthNumber, struct rainfall12M arrayRainfall[]);
+void printAverageRainfallPerMonthAcross5Years(char months[], struct rainfall12M arrayRainfall[]);
+
+/*
+struct rainfall calc() {} // returns a struct with format of arrayRainfall
+*/
+
+float calcTotalRainfallPerYear(struct rainfall12M year2k)
+{
+    return(year2k.inch1 + year2k.inch2 + year2k.inch3 + year2k.inch4 + year2k.inch5 + year2k.inch6 + year2k.inch7 + year2k.inch8 + year2k.inch9 + year2k.inch10 + year2k.inch11 + year2k.inch12);
+}
+
+void printTotalRainfallPerYear(int years[5], struct rainfall12M arrayRainfall[])
+{
     float totalPerYear[5];
-    float averagePerMonth[5];
-    float sumForAveragePerMonth = 0.0;
-    float sumForAveragePerMonthAcross5Years = 0.0;
-    float averagePerMonthAcross5YearsArr[12];
 
-    printf("year   totalPerYear   averagePerMonthIn1Year \n");
+    printf("year   totalPerYear\n");
 
-    for (outerCount = 0; outerCount < 5; outerCount++)
+    for(int yearNumber = 0; yearNumber < 5; yearNumber++)
     {
-        for (innerCount = 0; innerCount < 12; innerCount++)
-        {
-            sumForTotalPerYear += arr[outerCount][innerCount];
-            sumForAveragePerMonthAcross5Years += arr[innerCount][outerCount];
-        }
-        totalPerYear[outerCount] = sumForTotalPerYear;
-        averagePerMonth[outerCount] = averageRainfallPerMonthIn1Year(totalPerYear[outerCount]);
+        totalPerYear[yearNumber] = calcTotalRainfallPerYear(arrayRainfall[yearNumber]);
 
-        printTRPYAARPMI1Y(years[outerCount], totalPerYear[outerCount], averagePerMonth[outerCount]);
+        printf("%i     ", years[yearNumber]);
 
-        sumForAveragePerMonth += (averagePerMonth[outerCount])/5;
-        averagePerMonthAcross5YearsArr[outerCount] = sumForAveragePerMonthAcross5Years/5;
-        sumForTotalPerYear = 0.0;
+        // (totalPerYear[yearNumber]<100) ? printf("0%.4f \n", totalPerYear[yearNumber]) : printf("%.4f \n", totalPerYear[yearNumber]);
+
+        printf((totalPerYear[yearNumber]<100) ? "0%.4f \n" : "%.4f \n", totalPerYear[yearNumber]);
     }
-    printAverageRainfallPerYear(years, sumForAveragePerMonth);
-    averageRainfallPerMonthAcross5Years(months, averagePerMonthAcross5YearsArr, arr);
-}
 
-float averageRainfallPerMonthIn1Year(float totalPerYear)
-{
-    return totalPerYear/12;
-}
-
-float printTRPYAARPMI1Y(int years, float totalPerYear, float averagePerMonth)
-{
-    printf("%i     ", years);
-    (totalPerYear<100) ? printf("0%.4f           ", totalPerYear) : printf("%.4f           ", totalPerYear);
-    (averagePerMonth<10) ? printf("0%.4f \n", averagePerMonth) : printf("%.4f \n", averagePerMonth);
-}
-
-float printAverageRainfallPerYear(int years[5], float sumForAveragePerMonth)
-{
     printf("\n~*~*~*~*~*~*~*~*~*~*~*~*~ \n \n");
+}
 
+float calcAverageRainfallPerMonthIn1Year(struct rainfall12M year2k)
+{
+    return((calcTotalRainfallPerYear(year2k))/12);
+}
+
+void printAverageRainfallPerMonthIn1Year(int years[5], struct rainfall12M arrayRainfall[])
+{
+    float averagePerMonthIn1Year[5];
+
+    printf("year   averagePerMonthIn1Year \n");
+
+    for(int yearNumber = 0; yearNumber < 5; yearNumber++)
+    {
+        averagePerMonthIn1Year[yearNumber] = calcAverageRainfallPerMonthIn1Year(arrayRainfall[yearNumber]);
+
+        printf("%i         ", years[yearNumber]);
+
+        // (averagePerMonthIn1Year[yearNumber]<10) ? printf("0%.4f \n", averagePerMonthIn1Year[yearNumber]) : printf("%.4f \n", averagePerMonthIn1Year[yearNumber]);
+
+        printf((averagePerMonthIn1Year[yearNumber]<10) ? "0%.4f \n" : "%.4f \n", averagePerMonthIn1Year[yearNumber]);
+    }
+
+    printf("\n~*~*~*~*~*~*~*~*~*~*~*~*~ \n \n");
+}
+
+float calcAverageRainfallPerYear(struct rainfall12M arrayRainfall[])
+{
+    float sum = 0.0;
+
+    for(int yearNumber = 0; yearNumber < 5; yearNumber++)
+    {
+        sum += calcAverageRainfallPerMonthIn1Year(arrayRainfall[yearNumber]);
+    }
+
+    return (sum/5);
+}
+
+void printAverageRainfallPerYear(int years[5], struct rainfall12M arrayRainfall[])
+{
     printf("   year      averagePerYear \n");
-    printf("%i-%i       %.4f \n", years[0], years[4], sumForAveragePerMonth);
+    printf("%i-%i       %.4f \n", years[0], years[4], calcAverageRainfallPerYear(arrayRainfall));
+    printf("\n~*~*~*~*~*~*~*~*~*~*~*~*~ \n \n");
 }
 
-float averageRainfallPerMonthAcross5Years(char months[], float averagePerMonthAcross5YearsArr[], float arr[5][12])
+float calcAverageRainfallPerMonthAcross5Years(int monthNumber, struct rainfall12M arrayRainfall[])
 {
-    int outerCount;
-    float sum;
-    int innerCount;
+    float sum = 0.0;
 
-    printf("\n~*~*~*~*~*~*~*~*~*~*~*~*~ \n \n");
+    for(int index = 0 ; index < 4 ; index++)
+    {
+        if (monthNumber==0)
+        {
+            sum += (arrayRainfall[index].inch1);
+        }
+
+         else if (monthNumber==1)
+        {
+            sum += (arrayRainfall[index].inch2);
+        }
+
+        else if (monthNumber==2)
+        {
+            sum += (arrayRainfall[index].inch3);
+        }
+
+        else if (monthNumber==3)
+        {
+            sum += (arrayRainfall[index].inch4);
+        }
+
+        else if (monthNumber==4)
+        {
+            sum += (arrayRainfall[index].inch5);
+        }
+
+        else if (monthNumber==5)
+        {
+            sum += (arrayRainfall[index].inch6);
+        }
+
+        else if (monthNumber==6)
+        {
+            sum += (arrayRainfall[index].inch7);
+        }
+
+        else if (monthNumber==7)
+        {
+            sum += (arrayRainfall[index].inch8);
+        }
+
+        else if (monthNumber==8)
+        {
+            sum += (arrayRainfall[index].inch9);
+        }
+
+        else if (monthNumber==9)
+        {
+            sum += (arrayRainfall[index].inch10);
+        }
+
+        else if (monthNumber==10)
+        {
+            sum += (arrayRainfall[index].inch11);
+        }
+
+        else
+        {
+            sum += (arrayRainfall[index].inch12);
+        }
+
+        return sum;
+    }
+
+    /*
+    if (monthNumber==0)
+    {
+        return((arrayRainfall[0].inch1 + arrayRainfall[1].inch1 + arrayRainfall[2].inch1 + arrayRainfall[3].inch1 + arrayRainfall[4].inch1)/5);
+    }
+    else if (monthNumber==1)
+    {
+        return((arrayRainfall[0].inch2 + arrayRainfall[1].inch2 + arrayRainfall[2].inch2 + arrayRainfall[3].inch2 + arrayRainfall[4].inch2)/5);
+    }
+    else if (monthNumber==2)
+    {
+        return((arrayRainfall[0].inch3 + arrayRainfall[1].inch3 + arrayRainfall[2].inch3 + arrayRainfall[3].inch3 + arrayRainfall[4].inch3)/5);
+    }
+    else if (monthNumber==3)
+    {
+        return((arrayRainfall[0].inch4 + arrayRainfall[1].inch4 + arrayRainfall[2].inch4 + arrayRainfall[3].inch4 + arrayRainfall[4].inch4)/5);
+    }
+    else if (monthNumber==4)
+    {
+        return((arrayRainfall[0].inch5 + arrayRainfall[1].inch5 + arrayRainfall[2].inch5 + arrayRainfall[3].inch5 + arrayRainfall[4].inch5)/5);
+    }
+    else if (monthNumber==5)
+    {
+        return((arrayRainfall[0].inch6 + arrayRainfall[1].inch6 + arrayRainfall[2].inch6 + arrayRainfall[3].inch6 + arrayRainfall[4].inch6)/5);
+    }
+    else if (monthNumber==6)
+    {
+        return((arrayRainfall[0].inch7 + arrayRainfall[1].inch7 + arrayRainfall[2].inch7 + arrayRainfall[3].inch7 + arrayRainfall[4].inch7)/5);
+    }
+    else if (monthNumber==7)
+    {
+        return((arrayRainfall[0].inch8 + arrayRainfall[1].inch8 + arrayRainfall[2].inch8 + arrayRainfall[3].inch8 + arrayRainfall[4].inch8)/5);
+    }
+    else if (monthNumber==8)
+    {
+        return((arrayRainfall[0].inch9 + arrayRainfall[1].inch9 + arrayRainfall[2].inch9 + arrayRainfall[3].inch9 + arrayRainfall[4].inch9)/5);
+    }
+    else if (monthNumber==9)
+    {
+        return((arrayRainfall[0].inch10 + arrayRainfall[1].inch10 + arrayRainfall[2].inch10 + arrayRainfall[3].inch10 + arrayRainfall[4].inch10)/5);
+    }
+    else if (monthNumber==10)
+    {
+        return((arrayRainfall[0].inch11 + arrayRainfall[1].inch11 + arrayRainfall[2].inch11 + arrayRainfall[3].inch11 + arrayRainfall[4].inch11)/5);
+    }
+    else
+    {
+        return((arrayRainfall[0].inch12 + arrayRainfall[1].inch12 + arrayRainfall[2].inch12 + arrayRainfall[3].inch12 + arrayRainfall[4].inch12)/5);
+    }
+    */
+}
+
+void printAverageRainfallPerMonthAcross5Years(char months[], struct rainfall12M arrayRainfall[])
+{
+    float averagePerMonthAcross5Years[12];
 
     printf("averagePerMonthAcross5Years \n");
 
@@ -77,41 +235,36 @@ float averageRainfallPerMonthAcross5Years(char months[], float averagePerMonthAc
         printf(" %c%c%c   ", months[i], months[i+1], months[i+2]);
     }
 
-    // printf("Jan   Feb   Mar   Apr   May   Jun   Jul   Aug    Sep    Oct    Nov    Dec");
     printf("\n");
 
-    for (outerCount = 0; outerCount < 12; outerCount++)
+    for(int monthNumber = 0; monthNumber < 12; monthNumber++)
     {
-        sum = 0.0;
-        for (innerCount = 0; innerCount < 5; innerCount++)
-        {
-            sum += arr[innerCount][outerCount];
-        }
-        averagePerMonthAcross5YearsArr[outerCount] = sum/5;
-        (averagePerMonthAcross5YearsArr[outerCount]<10) ? printf("0%.2f  ", averagePerMonthAcross5YearsArr[outerCount]) : printf("%.2f  ", averagePerMonthAcross5YearsArr[outerCount]);
+        averagePerMonthAcross5Years[monthNumber] = calcAverageRainfallPerMonthAcross5Years(monthNumber, arrayRainfall);
+
+        // (averagePerMonthAcross5Years[monthNumber]<10) ? printf("0%.2f  ", averagePerMonthAcross5Years[monthNumber]) : printf("%.2f  ", averagePerMonthAcross5Years[monthNumber]);
+
+        printf((averagePerMonthAcross5Years[monthNumber]<10) ? "0%.2f  " : "%.2f  ", averagePerMonthAcross5Years[monthNumber]);
     }
 
-    printf("\n");
+    printf("\n \n~*~*~*~*~*~*~*~*~*~*~*~*~ \n \n");
 }
 
 int main()
 {
-    // 5 rows, 12 columns
-    // total rainfall per year, average rainfall per year, average rainfall per month
-
     int years[5] = {2000, 2001, 2002, 2003, 2004};
-
     char months[] = {"Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec"};
 
-    float arr[5][12] = {
-        {1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.10, 10.11, 11.12, 12.13},
-        {2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.10, 10.11, 11.12, 12.13, 13.14},
-        {3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.10, 10.11, 11.12, 12.13, 13.14, 14.15},
-        {4.5, 5.6, 6.7, 7.8, 8.9, 9.10, 10.11, 11.12, 12.13, 13.14, 14.15, 15.16},
-        {5.6, 6.7, 7.8, 8.9, 9.10, 10.11, 11.12, 12.13, 13.14, 14.15, 15.16, 16.17}
-    };
+    struct rainfall12M year2k0 = {1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.10, 10.11, 11.12, 12.13}; //type: struct
+    struct rainfall12M year2k1 = {2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.10, 10.11, 11.12, 12.13, 13.14}; //type: struct
+    struct rainfall12M year2k2 = {3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.10, 10.11, 11.12, 12.13, 13.14, 14.15}; //type: struct
+    struct rainfall12M year2k3 = {4.5, 5.6, 6.7, 7.8, 8.9, 9.10, 10.11, 11.12, 12.13, 13.14, 14.15, 15.16}; //type: struct
+    struct rainfall12M year2k4 = {5.6, 6.7, 7.8, 8.9, 9.10, 10.11, 11.12, 12.13, 13.14, 14.15, 15.16, 16.17}; //type: struct
 
-    totalRainfallPerYearAndAverageRainfallPerMonthIn1Year(years, months, arr);
+    //to create an array of structures:
+    struct rainfall12M arrayRainfall[] = {year2k0, year2k1, year2k2, year2k3, year2k4}; //type: array of structs
 
-    printf("\n~*~*~*~*~*~*~*~*~*~*~*~*~ \n \n");
+    printTotalRainfallPerYear(years, arrayRainfall);
+    printAverageRainfallPerMonthIn1Year(years, arrayRainfall);
+    printAverageRainfallPerYear(years, arrayRainfall);
+    printAverageRainfallPerMonthAcross5Years(months, arrayRainfall);
 }
