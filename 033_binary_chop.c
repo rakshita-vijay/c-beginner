0cb7eb3e3810d01cbc12fa8e941e13023a101700
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2023-06-05 14:07:40 +0530

    committing 033 and 034

--- 033_binary_chop.c
+++ 033_binary_chop.c
@@ -1,5 +1,6 @@
 // Binary Chop
 
+/*
 #include <stdio.h>
 
 int main() {
@@ -34,4 +35,59 @@ int main() {
   } else {
     printf("The number you entered, %i, is not found", numToSearch);
   }
+}
+*/
+
+// ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+#include <stdio.h>
+
+int main() {
+  int lenOfArrOfNum;
+  int numToSearch;
+  char result = 'n';
+  int index;
+
+  printf("Enter length of array of numbers: ");
+  scanf("%i", &lenOfArrOfNum);
+
+  int arrOfNum[lenOfArrOfNum];
+
+  printf("Enter the array values: \n");
+  for (index = 0; index < lenOfArrOfNum; index++) {
+    printf("%i> ", (index + 1));
+    scanf("%i", &arrOfNum[index]);
+  }
+
+  printf("Enter a number to search in the array of numbers: ");
+  scanf("%i", &numToSearch);
+
+  int mid;
+
+  if (lenOfArrOfNum % 2 == 0) {
+    mid = lenOfArrOfNum / 2;
+  } else {
+    mid = (lenOfArrOfNum + 1) / 2;
+  }
+
+  int halfArr[mid];
+
+  for (index = 0; index < mid; index++) {
+    halfArr[index] = arrOfNum[index];
+    if (numToSearch == arrOfNum[index]) {
+      result = 'y';
+    }
+  }
+
+  if (result != 'y') {
+    for (index = mid; index < lenOfArrOfNum; index++) {
+      halfArr[index - mid] = arrOfNum[index];
+    }
+  }
+
+  /*
+  for (index = 0; index < mid; index++) {
+    printf("%i", halfArr[index]);
+  }
+  */
 }
---
+++ 034_connect_four.c
@@ -0,0 +1,3 @@
+// Connect Four
+
+#include <stdio.h>
