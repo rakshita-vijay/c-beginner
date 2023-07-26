c59e7e4b81cc1f4c8b1900af1b9b214adfdce955
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2023-07-26 12:39:05 +0530

    036 - unfinished f()s drawRemainingCellsOnRightAsColumn & drawRemainingCellsOnRightAsRow

--- 036_chutes_and_ladders.c
+++ 036_chutes_and_ladders.c
@@ -49,66 +49,33 @@ void assignValuesToArrayOfCellValues(char arrayOfCellValues[numberOfCells][numbe
   // }
 }
 
-/*
-void drawBoard(char arrayOfCellValues[numberOfCells][numberOfPlayers], char playerTokens[]) {
-
-  printf("                                    Chutes and Ladders: A Game \n");
-
-  int countNumberOfPlayers = 0;
-  for (; countNumberOfPlayers < numberOfPlayers; countNumberOfPlayers++) {
-    printf("                                           Player %i: %c \n", (countNumberOfPlayers + 1), playerTokens[countNumberOfPlayers]);
-  }
-
-  printf("   _ _ _ _ _ \n");
-  printf("  /          \\_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _   \n");
-  printf(" |           |99   |98   |97   |96   |95   |94   |93   |92   |91   |90   |89   |88   |87   |86   |85   |84   /\\ \n");
-  printf(" |  FINISH!  |     |     |     |     |     |     |     |     |     |     |     |     |     |     |     |    /  \\ \n");
-  printf(" |           |_   _|_ _ _|_ _ _|_ _ _|_|_ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_   _|_ _ _|_ /_/|_ _/    \\ \n");
-  printf("  \\ _ _ _ _ /  ( (                   |_|                                               ) )         /_/     | 83  \\ \n");
-  printf("               ) )                   |_|                                              / /         /_/      /\\    / \n");
-  printf("              / /                    |_|                                             / /         /_/      /  \\  / \n");
-  printf("              ) )                    |_|                                            / /         /_/      /    \\/ \n");
-  printf("    _ _ _ _ _/ /_ _ _ _ _ _ _ _ _ _ _|_|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _( (_ _ _ _ _/_/_ _ _ /  82  | \n");
-  printf("   |    \\      |     |     |     |   |_|     |     |     |     |     |     |     |  \\ \\|     |/ /  |    \\      / \n");
-  printf("   | 65  \\ 66  | 67  | 68  | 69  |70 |_| 71  | 72  | 73  | 74  | 75  | 76  | 77  |78 | | 79  | 80  | 81  \\    / \n");
-  printf("    \\_ _ _\\ _ _|_ _ _|_ _ _|_ _ _|_ _|_|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _| |_ _ _|_ _ _|_ _ _|_ _/ \n");
-  printf("     \\     \\                         |_|                                            / /    _ _ _ _            \n");
-  printf("      \\ 64  \\_ _ _ _ _ _ _ _ _ _ _ _ |_| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\\ \\_ _/ _ _ _ \\_ _ _ _ _ _  \n");
-  printf("       \\    /     |     |     |     || |  |     |     |     |     |     |     |     |\\_ _ _/    |\\ \\    |     \\ \n");
-  printf("        \\  /  63  | 62  | 61  | 60  | 59  | 58  | 57  | 56  | 55  | 54  | 53  | 52  | 51  | 50  | ) )49 | 48   \\ \n");
-  printf("         \\/_ _ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_   _|_ _ _|_ _ _|_ _ _|_ _ _|_   _|_\\_\\_|_ _ _|_ _ _|/ / _ _|_ _ _ _\\ \n");
-  printf("                                            \\  \\ _ _/  \\       _ _ _ _ _ /  /    \\_\\            / /      \\       \\ \n");
-  printf("                                             \\_ _ _ _/\\ \\_ _ _/ _ _ _ _ _ _/      \\_\\          / /        \\  47   \\ \n");
-  printf("                    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\\_ _ _ _/_ _ _ _ _ _ _ _ _ _\\_\\_ _ _ _ _\\ \\_ _ _ _ /_ _ _ _/ \n");
-  printf("                   /\\       |     |     |     |     |     |     |     |     |     | \\ \\ |     |     |     |      / \n");
-  printf("                  /  \\ 32   | 33  | 34  | 35  | 36  | 37  | 38  | 39  | 40  | 41  | 42  | 43  | 44  | 45  | 46  / \n");
-  printf("                 /    \\_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ / \n");
-  printf("                / 31  /      _ _ _ _ _ _ _ _ _                _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  \n");
-  printf("               /_ _ _/      |     |     |     |              /     /     |     |     |     |     |     |     |     |     | \n");
-  printf("              /     /       | 23  | 22  | 21  |             / 15  /  14  | 13  | 12  | 11  | 10  | 09  | 08  | 07  | 06  | \n");
-  printf("             / 30  /        |_ _ _|_ _ _|_ _ _|            /_ _ _/_ _ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _| \n");
-  printf("            /_ _ _/         |     |     |     |           /     /                                                  |     | \n");
-  printf("           /     /          | 24  |     | 20  |          / 16  /                        _ _ _ _ _ _ _ _            | 05  | \n");
-  printf("          / 29  /           |_ _ _|     |_ _ _|         /_ _ _/                       /        __   .  \\_ _ _ _ _ _|_ _ _| \n");
-  printf("         /_ _ _/_ _ _ _ _ _/      |     |      \\_ _ _ _/     /                       |        /  \\ /|  |     |     |     | \n");
-  printf("        /     /     |     |  25   |     |   19  |     |  17  |                       |        |  |  |  | 02  | 03  | 04  | \n");
-  printf("       / 28  /  27  | 26  |      /       \\      | 18  |      |                       |        \\__/ _|_ |_ _ _|_ _ _|_ _ _| \n");
-  printf("      /_ _ _/_ _ _ _|_ _ _|_ _ _/         \\_ _ _|_ _ _|_ _ _/                         \\ _ _ _ _ _ _ _ _/ \n");
-}
-*/
-
 int promptForNumberOfCells(){
   // printf("Enter the number of cells you want in the game (Default is 100): ");
+  int num;
   printf("Enter the number of cells you want in the game: ");
-  scanf("%i", &numberOfCells);
+  scanf("%i", &num);
+  numberOfCells = (int) (floor(num));
   printf("\n");
 
   while ((numberOfCells > 200) || (numberOfCells < 10)) {
+    int choice;
+
     printf("Too ");
     (numberOfCells > 200) ? printf("many") : printf("less");
-    printf(" cells. Maximum is 200. Minimum is 10. Re-enter the number of cells: ");
-    scanf("%i", &numberOfCells);
+    printf(" cells. Maximum is 200. Minimum is 10. Default is 100. \nDo you want to: \n1) Re-enter the number of cells? \n2) Let it remain 100? \nEnter your choice: ");
+    scanf("%i", &choice);
     printf("\n");
+
+    if (choice==1){
+      printf("Re-enter the number of cells: ");
+      scanf("%i", &num);
+      numberOfCells = (int) (floor(num));
+      printf("\n");
+    }
+    else{
+      printf("The number of cells in the game is 100 \n");
+      numberOfCells=100;
+    }
   }
 
   return numberOfCells;
@@ -121,24 +88,45 @@ int numberOfCellsInABridge = 2;
 int numberOfCellsRemaining;
 
 void splitNumberOfCells(int numberOfCells){
-  numberOfCellsInARow = floor((numberOfCells - (numberOfBridges * numberOfCellsInABridge)) / numberOfRows);
+  numberOfCellsInARow = (int) (floor((numberOfCells - (numberOfBridges * numberOfCellsInABridge)) / numberOfRows));
   numberOfCellsRemaining = numberOfCells - ((numberOfCellsInARow * numberOfRows) + (numberOfBridges * numberOfCellsInABridge));
 }
 
-void drawRow(){
+void drawRow(int startIndex, char startSide) {
   printf("    ");
   for (int count = 0; count < (numberOfCellsInARow * 3); count++) {
     printf("_ ");
   }
   printf("\n");
 
-  for (int outerCount = 0; outerCount < 2; outerCount++) {
-    printf("   ");
-    for (int innerCount = 0; innerCount < numberOfCellsInARow; innerCount++) {
-      printf("|     ");
+  printf("   ");
+  int index;
+  if (startSide=='L') {
+    index = startIndex + numberOfCellsInARow;
+    for (int innerCount = 0; innerCount < numberOfCellsInARow; innerCount++, index--) {
+      if (index > 99){
+        printf("|%i  ", index);
+        } else{
+        (index > 9) ? printf("|%i   ", index) : printf("|0%i   ", index);
+        }
+      }
+  } else { // if (startSide=='R')
+      index = startIndex+1;
+      for (int innerCount = 0; innerCount < numberOfCellsInARow; innerCount++, index++) {
+        if (index > 99) {
+        printf("|%i  ", index);
+        } else {
+        (index > 9) ? printf("|%i   ", index) : printf("|0%i   ", index);
+        }
     }
-    printf("| \n");
   }
+  printf("| \n");
+
+  printf("   ");
+  for (int innerCount = 0; innerCount < numberOfCellsInARow; innerCount++) {
+    printf("|     ");
+  }
+  printf("| \n");
 
   printf("   ");
   for (int count = 0; count < numberOfCellsInARow; count++) {
@@ -147,14 +135,20 @@ void drawRow(){
   printf("| \n");
 }
 
-void drawRightBridge() {
+void drawRightBridge(int startIndex) {
+  int index = startIndex + numberOfCellsInABridge;
   for (int outerCount = 0; outerCount < numberOfCellsInABridge; outerCount++) {
-    for (int innerCount = 0; innerCount < 2; innerCount++) {
-      printf("   ");
-      for (int spaceCount1 = 0; spaceCount1 < ((numberOfCellsInARow-1) * 6); spaceCount1++) {
+    for (int lineCount = 0; lineCount < 2; lineCount++) {
+        printf("   ");
+        for (int spaceCount1 = 0; spaceCount1 < ((numberOfCellsInARow - 1) * 6); spaceCount1++) {
         printf(" ");
       }
-      printf("|     | \n");
+      if (index > 99){
+        (lineCount == 0) ? printf("|%i  | \n", (index - outerCount)) : printf("|    | \n");
+        }
+      else {
+        (lineCount == 0) ? ((index > 9) ? printf("|%i   | \n", (index - outerCount)) : printf("|0%i   | \n", (index - outerCount))) : ((index > 9) ? printf("|     | \n") : printf("|     | \n"));
+      }
     }
 
     printf("   ");
@@ -165,15 +159,77 @@ void drawRightBridge() {
   }
 }
 
-void drawLeftBridge() {
+void drawLeftBridge(int startIndex) {
+  int index = startIndex + numberOfCellsInABridge;
   for (int outerCount = 0; outerCount < numberOfCellsInABridge; outerCount++) {
-    for (int innerCount = 0; innerCount < 2; innerCount++) {
+    for (int lineCount = 0; lineCount < 2; lineCount++) {
       printf("   ");
-      printf("|     | \n");
+      if (index > 99) {
+        (lineCount == 0) ? printf("|%i  | \n", (index - outerCount)) : printf("|    | \n");
+      } else {
+        (lineCount == 0) ? ((index > 9) ? printf("|%i   | \n", (index - outerCount)) : printf("|0%i   | \n", (index - outerCount))) : ((index > 9) ? printf("|     | \n") : printf("|     | \n"));
+      }
+      // printf("|     | \n");
     }
 
     printf("   ");
     printf("|_ _ _| \n");
+  }
+}
+
+void drawRemainingCellsOnRightAsColumn(int numberOfCellsRemaining) {
+  for (; numberOfCellsRemaining > 0; numberOfCellsRemaining--) {
+    for (int lineCount = 0; lineCount < 2; lineCount++) {
+      printf("   ");
+      for (int spaceCount = 0; spaceCount < ((numberOfCellsInARow - 1) * 6); spaceCount++) {
+        printf(" ");
+      }
+      (lineCount == 0) ? (printf("|0%i   | \n", numberOfCellsRemaining)) : (printf("|     | \n"));
+    }
+
+    printf("   ");
+    for (int spaceCount2 = 0; spaceCount2 < ((numberOfCellsInARow - 1) * 6); spaceCount2++) {
+      printf(" ");
+    }
+    printf("|_ _ _| \n");
+  }
+}
+
+void drawRemainingCellsOnRightAsRow(int numberOfCellsRemaining) {
+  printf("    ");
+  for (int spaceCount = 0; spaceCount < ((numberOfCellsInARow - 1 - numberOfCellsRemaining) * 6); spaceCount++) {
+    printf(" ");
+  }
+
+  int index = numberOfCellsRemaining + 1;
+  for (int innerCount = 0; innerCount < numberOfCellsInARow; innerCount++, index++) {
+    (index > 9) ? printf("|%i   ", index) : printf("|0%i   ", index);
+  }
+  printf("| \n");
+
+  printf("   ");
+  for (int innerCount = 0; innerCount < numberOfCellsInARow; innerCount++) {
+    printf("|     ");
+  }
+  printf("| \n");
+
+  printf("   ");
+  for (int count = 0; count < numberOfCellsInARow; count++) {
+    printf("|_ _ _");
+  }
+  printf("| \n");
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  for (; numberOfCellsRemaining > 0; numberOfCellsRemaining--) {
+    printf("    ");
+    for (int lineCount = 0; lineCount < 2; lineCount++) {
+      printf("   ");
+      for (int spaceCount = 0; spaceCount < ((numberOfCellsInARow - 1 - numberOfCellsRemaining) * 6); spaceCount++) {
+        printf(" ");
+      }
+      printf("_ _ _ \n");
+    }
   }
 }
 
@@ -186,15 +242,44 @@ void drawBoard(char arrayOfCellValues[numberOfCells][numberOfPlayers], char play
     printf("                                           Player %i: %c \n", (countNumberOfPlayers + 1), playerTokens[countNumberOfPlayers]);
   }
 
-  for(int count =0; count< 2; count++){
-    drawRow();
-    drawRightBridge();
-    drawRow();
-    drawLeftBridge();
-  }
+  // for(int count =0; count< 2; count++){
+  //   drawRow();
+  //   drawRightBridge();
+  //   drawRow();
+  //   drawLeftBridge();
+  // }
 
-  drawRow();
-  drawRightBridge();
+  int startIndex = numberOfCells - numberOfCellsInARow;
+  drawRow(startIndex, 'L');
+
+  startIndex -= numberOfCellsInABridge;
+  drawRightBridge(startIndex);
+
+  startIndex -= numberOfCellsInARow;
+  drawRow(startIndex, 'R');
+
+  startIndex -= numberOfCellsInABridge;
+  drawLeftBridge(startIndex);
+
+  startIndex -= numberOfCellsInARow;
+  drawRow(startIndex, 'L');
+
+  startIndex -= numberOfCellsInABridge;
+  drawRightBridge(startIndex);
+
+  startIndex -= numberOfCellsInARow;
+  drawRow(startIndex, 'R');
+
+  startIndex -= numberOfCellsInABridge;
+  drawLeftBridge(startIndex);
+
+  startIndex -= numberOfCellsInARow;
+  drawRow(startIndex, 'L');
+
+  startIndex -= numberOfCellsInABridge;
+  drawRightBridge(startIndex);
+
+  (numberOfCellsRemaining<2) ? drawRemainingCellsOnRightAsColumn(numberOfCellsRemaining) : drawRemainingCellsOnRightAsRow(numberOfCellsRemaining);
 }
 
 bool askIfPlayerWantsToPlay(int playerNumber) {
@@ -282,3 +367,51 @@ int main() {
   // for (; arrayOfCellValues[indexOfArrayOfCellValues][0] != '\0', indexOfArrayOfCellValues++) {
   // }
 }
+
+/*
+void drawBoard(char arrayOfCellValues[numberOfCells][numberOfPlayers], char playerTokens[]) {
+
+  printf("                                    Chutes and Ladders: A Game \n");
+
+  int countNumberOfPlayers = 0;
+  for (; countNumberOfPlayers < numberOfPlayers; countNumberOfPlayers++) {
+    printf("                                           Player %i: %c \n", (countNumberOfPlayers + 1), playerTokens[countNumberOfPlayers]);
+  }
+
+  printf("   _ _ _ _ _ \n");
+  printf("  /          \\_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _   \n");
+  printf(" |           |99   |98   |97   |96   |95   |94   |93   |92   |91   |90   |89   |88   |87   |86   |85   |84   /\\ \n");
+  printf(" |  FINISH!  |     |     |     |     |     |     |     |     |     |     |     |     |     |     |     |    /  \\ \n");
+  printf(" |           |_   _|_ _ _|_ _ _|_ _ _|_|_ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_   _|_ _ _|_ /_/|_ _/    \\ \n");
+  printf("  \\ _ _ _ _ /  ( (                   |_|                                               ) )         /_/     | 83  \\ \n");
+  printf("               ) )                   |_|                                              / /         /_/      /\\    / \n");
+  printf("              / /                    |_|                                             / /         /_/      /  \\  / \n");
+  printf("              ) )                    |_|                                            / /         /_/      /    \\/ \n");
+  printf("    _ _ _ _ _/ /_ _ _ _ _ _ _ _ _ _ _|_|_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _( (_ _ _ _ _/_/_ _ _ /  82  | \n");
+  printf("   |    \\      |     |     |     |   |_|     |     |     |     |     |     |     |  \\ \\|     |/ /  |    \\      / \n");
+  printf("   | 65  \\ 66  | 67  | 68  | 69  |70 |_| 71  | 72  | 73  | 74  | 75  | 76  | 77  |78 | | 79  | 80  | 81  \\    / \n");
+  printf("    \\_ _ _\\ _ _|_ _ _|_ _ _|_ _ _|_ _|_|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _| |_ _ _|_ _ _|_ _ _|_ _/ \n");
+  printf("     \\     \\                         |_|                                            / /    _ _ _ _            \n");
+  printf("      \\ 64  \\_ _ _ _ _ _ _ _ _ _ _ _ |_| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\\ \\_ _/ _ _ _ \\_ _ _ _ _ _  \n");
+  printf("       \\    /     |     |     |     || |  |     |     |     |     |     |     |     |\\_ _ _/    |\\ \\    |     \\ \n");
+  printf("        \\  /  63  | 62  | 61  | 60  | 59  | 58  | 57  | 56  | 55  | 54  | 53  | 52  | 51  | 50  | ) )49 | 48   \\ \n");
+  printf("         \\/_ _ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_   _|_ _ _|_ _ _|_ _ _|_ _ _|_   _|_\\_\\_|_ _ _|_ _ _|/ / _ _|_ _ _ _\\ \n");
+  printf("                                            \\  \\ _ _/  \\       _ _ _ _ _ /  /    \\_\\            / /      \\       \\ \n");
+  printf("                                             \\_ _ _ _/\\ \\_ _ _/ _ _ _ _ _ _/      \\_\\          / /        \\  47   \\ \n");
+  printf("                    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\\_ _ _ _/_ _ _ _ _ _ _ _ _ _\\_\\_ _ _ _ _\\ \\_ _ _ _ /_ _ _ _/ \n");
+  printf("                   /\\       |     |     |     |     |     |     |     |     |     | \\ \\ |     |     |     |      / \n");
+  printf("                  /  \\ 32   | 33  | 34  | 35  | 36  | 37  | 38  | 39  | 40  | 41  | 42  | 43  | 44  | 45  | 46  / \n");
+  printf("                 /    \\_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ / \n");
+  printf("                / 31  /      _ _ _ _ _ _ _ _ _                _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  \n");
+  printf("               /_ _ _/      |     |     |     |              /     /     |     |     |     |     |     |     |     |     | \n");
+  printf("              /     /       | 23  | 22  | 21  |             / 15  /  14  | 13  | 12  | 11  | 10  | 09  | 08  | 07  | 06  | \n");
+  printf("             / 30  /        |_ _ _|_ _ _|_ _ _|            /_ _ _/_ _ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _|_ _ _| \n");
+  printf("            /_ _ _/         |     |     |     |           /     /                                                  |     | \n");
+  printf("           /     /          | 24  |     | 20  |          / 16  /                        _ _ _ _ _ _ _ _            | 05  | \n");
+  printf("          / 29  /           |_ _ _|     |_ _ _|         /_ _ _/                       /        __   .  \\_ _ _ _ _ _|_ _ _| \n");
+  printf("         /_ _ _/_ _ _ _ _ _/      |     |      \\_ _ _ _/     /                       |        /  \\ /|  |     |     |     | \n");
+  printf("        /     /     |     |  25   |     |   19  |     |  17  |                       |        |  |  |  | 02  | 03  | 04  | \n");
+  printf("       / 28  /  27  | 26  |      /       \\      | 18  |      |                       |        \\__/ _|_ |_ _ _|_ _ _|_ _ _| \n");
+  printf("      /_ _ _/_ _ _ _|_ _ _|_ _ _/         \\_ _ _|_ _ _|_ _ _/                         \\ _ _ _ _ _ _ _ _/ \n");
+}
+*/
