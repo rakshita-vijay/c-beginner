6a9405c42845948e57d99ec7dfd23296bd680cd6
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2024-01-06 10:15:26 +0530

    pointer_notes - added notes

--- pointer_notes.c
+++ pointer_notes.c
@@ -25,7 +25,7 @@ print a;  // prints value of a
 print &a;  // prints address of a (& = address)
 
 if you do:
-*p = 8;  // a = 8
+*p = 8;  // a = 8 ---> dereferencing
 print a;  // 8
 
 
@@ -33,3 +33,9 @@ int num = 5;
 int *p1 = &num;
 char alph = 'c';
 char *p2 = &alph;
+
+p = p+1 increments the pointer address by 4 bytes
+*p = (*p) + 1 increments the value at the address stored in p by 1;
+
+Address : &A[i] or (A + i) // simply using A gives base address
+Value : A[i] or *(A + i)
