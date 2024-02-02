873be5ac784807cbce0aa8be734eaf1d0d54073d
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2024-02-02 14:15:00 +0530

    Update 037_taylor_series.c

--- 037_taylor_series.c
+++ 037_taylor_series.c
@@ -2,18 +2,20 @@
 #include <math.h>
 
 double fact(int count) {
-  return count * fact(count - 1);
+  return (count == 1) ? 1 : (count * fact(count - 1));
 }
 
-float val(int num) {
-  long int count = 1;
-  return (pow(-1, (count-1)))*((pow(num, count))/fact(count)) + val(count + 1);
+float val(int num, long int count) {
+  return (pow(-1, (count + 1))) * ((pow(num, count)) / fact(count)) + val(num, count + 2);
 }
 
 void main() {
   int num;
+  long int count = 1;
+  
   printf("Enter a number: ");
   scanf("%d", &num);
-  float result = val(num);
+  
+  float result = val(num, count);
   printf("The result is: %f", result);
 }
