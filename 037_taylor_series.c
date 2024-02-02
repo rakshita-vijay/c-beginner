f01927cf8558e08ed88aa56b663a0b7f9f712772
Rakshita Vijay <85537286+rakshita-vijay@users.noreply.github.com>
2024-02-02 14:21:05 +0530

    Update 037_taylor_series.c

--- 037_taylor_series.c
+++ 037_taylor_series.c
@@ -5,17 +5,21 @@ double fact(int count) {
   return (count == 1) ? 1 : (count * fact(count - 1));
 }
 
-float val(int num, long int count) {
-  return (pow(-1, (count + 1))) * ((pow(num, count)) / fact(count)) + val(num, count + 2);
+float val(int num, long int count, long int power_for_sign) {
+  int sign = (pow(-1, (power_for_sign + 1)));
+  int numerator = (pow(num, count));
+  int denomenator = fact(count);
+  return sign * (numerator / denomenator) + val(num, count + 2, power_for_sign + 1);
 }
 
 void main() {
   int num;
   long int count = 1;
+  long int power_for_sign = 1;
   
   printf("Enter a number: ");
   scanf("%d", &num);
   
-  float result = val(num, count);
+  float result = val(num, count, power_for_sign);
   printf("The result is: %f", result);
 }
