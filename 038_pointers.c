776e700e57a204e3377deec6851d99ae992358e6
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2024-02-22 09:18:21 +0530

    038 - added content

--- 038_pointers.c
+++ 038_pointers.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+void main() {
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+
+  int *p_num;
+  p_num = &num;
+
+  printf("Number entered: %d", num);
+  printf("Number's address: %d", &num);
+
+}
