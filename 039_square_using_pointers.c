68494cea0b811b023310d23976f3e743af8a03b5
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2024-02-22 16:38:41 +0530

    039 - included version 2

--- 039_square_using_pointers.c
+++ 039_square_using_pointers.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-int square(int *p_num) {
-  return pow(*p_num, 2);
+int square(int *p_num_loc) {
+  return pow(*p_num_loc, 2);
 }
 
 int main () {
@@ -15,4 +15,27 @@ int main () {
 
   int res = square(p_num);
   printf("Square of %d is %d \n", *p_num, res);
+}
+
+// ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+#include <stdio.h>
+#include <math.h>
+
+void square(int *p_num_loc) {
+  *p_num_loc *= *p_num_loc;
+}
+
+int main () {
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+
+  int copy_of_num = num;
+
+  int *p_num;
+  p_num = &num;
+
+  square(p_num);
+  printf("Square of %d is %d \n", copy_of_num, *p_num);
 }
