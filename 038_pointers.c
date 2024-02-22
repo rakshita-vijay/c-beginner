b2a024745fc03f8ec3a988e51be3cbe66b783ce0
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2024-02-22 09:21:30 +0530

    038 - added pointer bits

--- 038_pointers.c
+++ 038_pointers.c
@@ -8,7 +8,9 @@ void main() {
   int *p_num;
   p_num = &num;
 
-  printf("Number entered: %d", num);
-  printf("Number's address: %d", &num);
-
+  printf("Number's value: %d \n", num);
+  printf("Number's address: %d \n \n", &num);
+  printf("Pointer: %p \n", p_num);
+  printf("Pointer's address: %d \n", &p_num);
+  printf("Pointer's value: %d \n", *p_num);
 }
