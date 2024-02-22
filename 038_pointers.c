098adb650127f028806645925ff6b7d9d42fbab8
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2024-02-22 10:46:06 +0530

    038 - rectified int to pointer format specifier; git notes - added alternative to compile and run

--- 038_pointers.c
+++ 038_pointers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void main() {
+int main () {
   int num;
   printf("Enter a number: ");
   scanf("%d", &num);
@@ -9,10 +9,15 @@ void main() {
   p_num = &num;
 
   printf("\nNumber entered: %d \n", num);
-  printf("Number's address: %d \n \n", &num);
+  // printf("Number's address: %d \n \n", &num);
+  printf("Number's address: %p \n \n", &num);
 
-  printf("Pointer's value: %d \n", p_num);
+  // printf("Pointer's value: %d \n", p_num);
+  printf("Pointer's value: %p \n", p_num);
   printf("Pointer's value in hexadecimal: %p \n", p_num);
-  printf("Pointer's address: %d \n", &p_num);
-  printf("Pointer is pointing at: %d through number address: %d \n", *p_num, &num);
+  // printf("Pointer's address: %d \n", &p_num);
+  printf("Pointer's address: %p \n", &p_num);
+  // printf("Pointer is pointing at: %d through number address: %d \n", *p_num, &num);
+  printf("Pointer is pointing at: %d through number address: %p \n", *p_num, &num);
+
 }
