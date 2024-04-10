f6b30ec63b418f06c6050b6248461de19bebd529
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2024-04-10 20:39:18 +0530

    045.3 - navigating nxn matrix based on value in cell, code not working yet

--- 045.3_varun_practice_sheet_question_3.c
+++ 045.3_varun_practice_sheet_question_3.c
@@ -21,3 +21,78 @@
 // 80
 
 // (You go right, right, down, down, and left in this case.)
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int num_of_r_c;
+  int number_of_steps;
+
+  printf("Enter the number of rows and columns in an nxn matrix: ");
+  scanf("%i", &num_of_r_c);
+  printf("Enter the number of steps: ");
+  scanf("%i", &number_of_steps);
+  printf("\n");
+
+  int matrix[num_of_r_c][num_of_r_c];
+  for (int row_count = 0; row_count < num_of_r_c; row_count++) {
+    for (int column_count = 0; column_count < num_of_r_c; column_count++) {
+      printf("Enter the value of %dx%d: ", row_count + 1, column_count + 1);
+      scanf("%i", &matrix[row_count][column_count]);
+    }
+  }
+
+  printf("\n");
+
+  printf("Your matrix is: \n");
+  for (int row_count = 0; row_count < num_of_r_c; row_count++) {
+    for (int column_count = 0; column_count < num_of_r_c; column_count++) {
+      printf("%3i  ", matrix[row_count][column_count]);
+    }
+    printf("\n");
+  }
+
+  int current_cell[2] = {0, 0};
+  int output_printed = 0;
+  int step_count = 0;
+  while (step_count < number_of_steps) {
+    for (int row_count = 0; row_count < num_of_r_c; row_count++) {
+      for (int column_count = 0; column_count < num_of_r_c; column_count++) {
+        if ((matrix[row_count][column_count] % 4) == 0) {
+          if (row_count == 0) {
+            output_printed = -1;
+            break;
+          } else {
+            current_cell[0] -= 1;
+          }
+        } else if ((matrix[row_count][column_count] % 4) == 1) {
+          if (column_count == num_of_r_c - 1) {
+            output_printed = -1;
+            break;
+          } else {
+            current_cell[1] += 1;
+          }
+        } else if ((matrix[row_count][column_count] % 4) == 2) {
+          if (row_count == num_of_r_c - 1) {
+            output_printed = -1;
+            break;
+          } else {
+            current_cell[0] += 1;
+          }
+        } else {
+          if (column_count == 0) {
+            output_printed = -1;
+            break;
+          } else {
+            current_cell[1] -= 1;
+          }
+        }
+      }
+    }
+  step_count++;
+  }
+
+  printf("\n");
+  (output_printed == -1) ? printf("Exceeded row/column range: %d \n", output_printed) : printf("You are at %dx%d and the value is: %d \n", current_cell[0], current_cell[1], matrix[current_cell[0]][current_cell[1]]);
+}
