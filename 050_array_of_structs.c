7a77e66e7c92c0b11ce0247a936c783a0614e6d6
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2024-04-12 15:37:14 +0530

    050 - output coming, not aligned, rough working code

--- 050_array_of_structs.c
+++ 050_array_of_structs.c
@@ -12,7 +12,7 @@ int main() {
   int num_of_students;
   printf("Enter the number of students: ");
   scanf("%d", &num_of_students);
-  fflush(stdin);
+  getchar();
   printf("\n");
 
   struct student stud_arr[num_of_students];
@@ -21,7 +21,7 @@ int main() {
   for (int count = 0; count < num_of_students; count++) {
     printf("Enter student #%d's name: ", count + 1);
     scanf("%[^\n]", stud_arr[count].name);
-    fflush(stdin);
+    getchar();
 
     printf("Enter student #%d's ID number: ", count + 1);
     scanf("%d", &stud_arr[count].ID_number);
@@ -29,7 +29,7 @@ int main() {
     printf("Enter student #%d's aggregate marks: ", count + 1);
     scanf("%f", &stud_arr[count].agg_marks);
     printf("\n");
-    fflush(stdin);
+    getchar();
   }
 
   // printing user_input in form of a table
@@ -40,32 +40,32 @@ int main() {
     }
   }
 
-  printf("length_of_max_name = %d", length_of_max_name);
+  printf("length_of_max_name = %d\n", length_of_max_name);
 
-  for (int counter = 0; counter < (length_of_max_name - strlen("Name")); counter++) {
+  for (int counter = 0; counter < (int) (length_of_max_name - strlen("Name")); counter++) {
     printf(" ");
   }
   printf("Name");
-  for (int counter = 0; counter < (length_of_max_name - strlen("ID number")); counter++) {
+  for (int counter = 0; counter < (int) (length_of_max_name - strlen("ID number")); counter++) {
     printf(" ");
   }
   printf("ID number");
-  for (int counter = 0; counter < (length_of_max_name - strlen("Aggregate Marks")); counter++) {
+  for (int counter = 0; counter < 6; counter++) {
     printf(" ");
   }
   printf("Aggregate Marks");
   printf("\n");
 
   for (int count = 0; count < num_of_students; count++) {
-    for (int counter = 0; counter < length_of_max_name - strlen(stud_arr[count].name); counter ++) {
+    for (int counter = 0; counter < (int) (length_of_max_name - strlen(stud_arr[count].name)); counter++) {
       printf(" ");
     }
     printf("%s", stud_arr[count].name);
-    for (int counter = 0; counter < length_of_max_name - log10(stud_arr[count].ID_number) + 1; counter ++) {
+    for (int counter = 0; counter < (int) (length_of_max_name + strlen("ID number") - log10(stud_arr[count].ID_number) + 1); counter++) {
       printf(" ");
     }
     printf("%i", stud_arr[count].ID_number);
-    for (int counter = 0; counter < length_of_max_name - log10(stud_arr[count].agg_marks) + 1; counter ++) {
+    for (int counter = 0; counter < (int) (strlen("  Aggregate Marks") - log10(stud_arr[count].agg_marks) + 1); counter++) {
       printf(" ");
     }
     printf("%f", stud_arr[count].agg_marks);
