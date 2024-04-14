106f7e2628c4457f7e82f8be4584ca00e6a91bc8
Rakshita Vijay <rakshita-vijay@users.noreply.github.com>
2024-04-14 11:02:25 +0530

    052 - working code for all

--- 052_pyqs.c
+++ 052_pyqs.c
@@ -1,4 +1,4 @@
-// Q1
+// Q1 - Umaang
 
 // #include <stdio.h>
 // #include <string.h>
@@ -68,46 +68,104 @@
 //   }
 // }
 
+// ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+// Q2 - Umaang
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main () {
+//   int num_of_strings;
+//   printf("Enter the number of strings: ");
+//   scanf("%d", &num_of_strings);
+//   printf("\n");
+
+//   char arr[20];
+//   int length_of_arr;
+//   int counter = 1;
+
+//   for (int string_number = 0; string_number < num_of_strings; string_number++) {
+//     printf("Enter string #%i [end of string is considered a backslash]: ", string_number + 1);
+//     scanf("%s", arr);
+
+//     length_of_arr = strlen(arr);
+
+//     for (int holder = 0; holder < length_of_arr; holder++) {
+//       for (int index = holder + 1; index < length_of_arr; index++) {
+//         if (arr[holder] == arr[index]) {
+//           counter++;
+//         }
+//         if (counter > 2) {
+//           break;
+//         }
+//       }
+//       if (counter > 2) {
+//         printf("%i %s \n", holder, arr);
+//         break;
+//       } else {
+//         counter = 1;
+//       }
+//     }
+//     printf("\n");
+//   }
+// }
 
 // ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
 
-// Q2
+// Q1 - Rajas Bamb
+
+// #include <stdio.h>
+
+// int main () {
+//   int pf_num;
+//   printf("Enter the prime factor number: ");
+//   scanf("%d", &pf_num);
+
+//   int pf_num_square = pf_num * pf_num;
+
+//   int n;
+//   printf("Enter n for the nth number: ");
+//   scanf("%d", &n);
+
+//   int arr[n];
+
+//   for (int index = 0; index < n; ) {
+//     for (int num_to_check = pf_num_square; index < n; num_to_check++) {
+//       if (((num_to_check % pf_num) == 0) && ((num_to_check % pf_num_square) == 0)) {
+//         arr[index] = num_to_check;
+//         index++;
+//       }
+//     }
+//   }
+
+//   printf("%dth number is %d, and prime factor: %d \n", n, arr[n - 1], pf_num);
+// }
+
+// ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+// Q2 - Rajas Bamb
 
 #include <stdio.h>
-#include <string.h>
 
 int main () {
-  int num_of_strings;
-  printf("Enter the number of strings: ");
-  scanf("%d", &num_of_strings);
-  printf("\n");
+  int num_of_lines;
+  printf("Enter the number of lines: ");
+  scanf("%d", &num_of_lines);
 
-  char arr[20];
-  int length_of_arr;
-  int counter = 1;
-
-  for (int string_number = 0; string_number < num_of_strings; string_number++) {
-    printf("Enter string #%i [end of string is considered a backslash]: ", string_number + 1);
-    scanf("%s", arr);
-
-    length_of_arr = strlen(arr);
-
-    for (int holder = 0; holder < length_of_arr; holder++) {
-      for (int index = holder + 1; index < length_of_arr; index++) {
-        if (arr[holder] == arr[index]) {
-          counter++;
-        }
-        if (counter > 2) {
-          break;
-        }
+  for (int line_count = 0; line_count <= num_of_lines; line_count++) {
+    // left spaces
+    for (int space_count = 0; space_count < num_of_lines - line_count; space_count++) {
+      printf("   ");
+    }
+    // numbers
+    int num_to_print = 1;
+    for(int num_of_nums_to_print = 1 ; num_of_nums_to_print <= line_count ;){
+      printf("    %d", num_to_print);
+      num_of_nums_to_print++;
+      num_to_print *= num_of_nums_to_print;
       }
-      if (counter > 2) {
-        printf("%i %s \n", holder, arr);
-        break;
-      } else {
-        counter = 1;
-      }
+      printf("\n");
     }
     printf("\n");
   }
-}
